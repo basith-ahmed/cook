28204d594d38d56cd3004a3ab71de3533a9dccba
Basith Ahmed N A <basithahmeddd@gmail.com>
2024-08-07 01:11:40 +0530

    routes

---
+++ routes/cook.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+const dotenv = require('dotenv').config();
+const getImage = require("../middleware/getImage");
+
+const router = express.Router();
+
+const key = process.env.API_KEY;
+
+router.get("/", (req, res) => {
+  res.status(200);
+  res.send("You have reached the kitchen! Lets cook something!");
+});
+
+router.get("/:animal", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(200);
-  res.send("Hey Chef!");
+  res.send("Hey Chef! Let's cook something, head over to /cook");
 });
+
+app.use("/cook", cookRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
