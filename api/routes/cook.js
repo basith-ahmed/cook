6fd892961835ce787886159abfa2a7745d016d96
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-08-07 16:23:24 +0530

    be edit

--- api/routes/cook.js
+++ api/routes/cook.js
@@ -9,7 +9,7 @@ const key = process.env.API_KEY;
 
 router.get("/", (req, res) => {
   res.status(200);
-  res.send("You have reached the kitchen! Lets cook something!");
+  res.send("You have reached the kitchen! Lets cook something!\n Use /cook/:anything to get an image of anything!");
 });
 
 router.get("/:animal", getImage, (req, res) => {});
